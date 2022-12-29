aa47487700c8f2df9d28f6d38e43ce6acbba4983
VarunSalaria12219988 <119073114+VarunSalaria9@users.noreply.github.com>
2022-12-29 23:09:12 +0530

    Add files via upload

---
+++ lecture18.py
@@ -0,0 +1,22 @@
+#variable in python
+number1=2
+print(number1)
+
+number1=4
+print(number1)
+
+name="Sagar"
+print(name)
+
+name=123
+print(name)
+
+_name="Sagar"
+print(_name)
+
+user_one_name="Sagar"   #snake case writing
+print(user_one_name)
+
+userOneName="Sagar"   #camel case writing
+print(userOneName)
+
